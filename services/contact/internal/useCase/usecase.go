624d2e9937d217b0af8784473952f670f2703b20
eegurt <alihan28.06.0450@gmail.com>
2023-06-08 20:55:00 +0600

    slide 28: usecase interface implementation

--- services/contact/internal/useCase/usecase.go
+++ services/contact/internal/useCase/usecase.go
@@ -22,3 +22,30 @@ type useCase struct {
 func New(repo repository.Repo) *useCase {
 	return &useCase{repo: repo}
 }
+func (u *useCase) CreateContact(user *domain.Contact) (int, error) {
+	return 0, nil
+}
+
+func (u *useCase) ReadContact(id int) (*domain.Contact, error) {
+	return nil, nil
+}
+
+func (u *useCase) UpdateContact(id int) error {
+	return nil
+}
+
+func (u *useCase) DeleteContact(id int) error {
+	return nil
+}
+
+func (u *useCase) CreateGroup(group *domain.Group) (int, error) {
+	return 0, nil
+}
+
+func (u *useCase) ReadGroup(id int) (*domain.Group, error) {
+	return nil, nil
+}
+
+func (u *useCase) AddContact(id int) error {
+	return nil
+}
