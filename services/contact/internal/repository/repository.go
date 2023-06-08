33e6986cbec825485aadc35c0d3d841952eef93e
eegurt <alihan28.06.0450@gmail.com>
2023-06-08 17:03:59 +0600

    slide 18: repository interface

--- services/contact/internal/repository/repository.go
+++ services/contact/internal/repository/repository.go
@@ -1,17 +1,17 @@
-package usecase
+package repository
 
 import (
 	"eegurt.go-clean-arch/services/contact/internal/domain"
 )
 
-type ContactUsecase interface {
+type ContactRepo interface {
 	Create(user *domain.Contact) (int, error)
 	Read(id int) (*domain.Contact, error)
 	Update(id int) error
 	Delete(id int) error
 }
 
-type GroupUsecase interface {
+type GroupRepo interface {
 	Create(group *domain.Group) (int, error)
 	Read(id int) (*domain.Group, error)
 	AddContact(id int) error
